55d189d8e1135e6f8f3ee291d436338ac8850514
Ahint <91521097+Ahint@users.noreply.github.com>
2022-04-16 17:57:09 +0530

    Add files via upload

---
+++ Area of Circle.py
@@ -0,0 +1,4 @@
+r=input("Value of radius: ")
+r=int(r)
+A=3.14*r**2
+print("Area of Circle: ",A)
---
+++ Area of Rectangle.py
@@ -0,0 +1,6 @@
+l=input("Value of length: ")
+l=int(l)
+b=input("Value of breadth: ")
+b=int(b)
+A=l*b
+print("Area of Rectangle: ",A)
